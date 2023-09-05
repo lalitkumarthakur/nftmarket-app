1d837c6b24513b7bbc4dcba82eb74e9ebeb18e2c
lalitkumarthakur <lkt929129@gmail.com>
2023-09-05 21:54:27 +0530

    imported stackNavigator and navigationContainer

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My First React Native Application ! </Text>
-      <StatusBar style="auto" />
+      <Text> Hello World </Text>
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
