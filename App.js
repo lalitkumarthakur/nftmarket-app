f5395ad82ebd4b652ecd813bd774503a4468a336
lalitkumarthakur <lkt929129@gmail.com>
2023-09-05 21:56:27 +0530

    removed not required elements

--- App.js
+++ App.js
@@ -2,23 +2,10 @@
 // import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
+import { useFonts } from "/screen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
       <Text> Hello World </Text>
-      {/* <StatusBar style="auto" /> */}
-    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FF9933',
-    fontWeight: '700',
-    color: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
